1d8c1fc1f81378ee417fd175be71eed1e30748a3
Arjay Jean <thearjayjean@gmail.com>
2023-01-25 11:33:36 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,25 +1,43 @@
-# ComiXology's Weekly Featured New Releases ETL
+<h1 align="center">ComiXology's Weekly Featured New Releases ETL</h1>
+
+<h3 align="center"><u>🧰 Languages and Tools</u></h3>
+<p align="center">
+<img src="https://user-images.githubusercontent.com/97479656/214594812-e19961c9-00cd-4c7a-9c56-5b8b45f9ed13.png">
+</p>
+<hr>
 
 ### Purpose:
-* Every week, there are new comicbooks getting release on Amazon's cloud-based digital distribution platform for comics, [ComiXology](https://www.amazon.com/kindle-dbs/comics-store/home/ "Amazon's ComiXology"). As a comicbook enthusiast, I wanted to create an ETL process that is automated using AWS services, load the data in a CSV file, and would store the CSV file of the weekly releases in Amazon S3. In order to do so, I used AWS Step Functions to create a cron job that will be called upon every Monday at 6pm 
+* Every week, there are new comicbooks getting release on Amazon's cloud-based digital distribution platform for comics, [ComiXology](https://www.amazon.com/kindle-dbs/comics-store/home/ "Amazon's ComiXology"). As a comicbook enthusiast, I wanted to create an ETL process that is automated using AWS services, extracts the appropriate data, is transformed by Python, loaded itno a CSV file, and would be stored Amazon S3.
+#
 
+<div>
 
-### Tools:
-* Python
-* Scrapy (Python Package)
-* AWS Lambda
-* AWS Step Functions
-* Amazon EventBridge Schedule
-* Amazon Simple Storage Service (S3)
-* Amazon Simple Notifaction Service (SNS)
+<table align="center">
 
+<tr>
+<td>⚙ ETL Process:</td>
+<td>AWS Diagram</td>
+</tr>
 
-### ETL Process:
-1. Every Monday at 6pm, a cron job using EventBridge Schedule will call for a Step Function workflow to collect [ComiXology's](https://www.amazon.com/kindle-dbs/comics-store/home/ "Amazon's ComiXology"), an Amazon cloud-based digital distribution platform for comics, weekly featured new releases.
-2. Withing that workflow, there are two Lambda function will trigger a Python script that will web scrape  
-3. Another Lambda function will trigger a message dir
-4. 
-5. 
+<tr>
+<td width="550px">
+<ol>
+<li>Every Tuesday at 9am, a cron job using EventBridge Schedule will call for a Lambda function that is an ETL process created with Python to collect <a href="https://www.amazon.com/kindle-dbs/comics-store/home/" target="_blank">ComiXology's</a>, an Amazon cloud-based digital distribution platform for comics, weekly featured new releases.</li>
+<li>The ETL process will start with the extraction of the data using BeautifulSoup.</li>
+<li>Once the data has been extracted, there will be a process of cleaning and formatting the data so that it can be loaded into a CSV file</li>
+<li>After the cleaning, a CSV file will be created, then the data will be loaded into it (Which will end the ETL process).</li>
+<li>With the completion of the ETL process, the recently created CSV file will be stored in AWS S3. This is possible with Boto3, an AWS SDK for Python (This will be the end of the first Lambda function).</li>
+<li>The storing of the file will then trigger another Lambda function that was set to be triggered when an object has been created.</li> 
+<li>Once triggered, an email will be sent to me confirming that the file has been successfully uploaded and that the Comixology featured new releases has been stored(Ends the second and last Lambda function).</li> 
+</ol>
+</td>
 
-![comixology_new_releases drawio](https://user-images.githubusercontent.com/97479656/214044955-92951e6b-d952-4048-a5b9-a54cc75e82a5.png)
+<td>
+<img src="https://user-images.githubusercontent.com/97479656/214612607-92ae8e51-801c-4621-a67b-536d618e47a8.png">
+</td>
+</tr>
+
+</table>
+
+</div>
 
--- scripts/etl.py
+++ scripts/etl.py
@@ -6,9 +6,7 @@ import datetime
 import boto3
 subprocess.run('clear')
 
-# def lambda_handler():
-
-# Extracting data from Amazon's Comixology page using Beautiful Soup
+"""Extracting data from Amazon's Comixology page using Beautiful Soup"""
 URL = 'https://www.amazon.com/kindle-dbs/comics-store/home/?ref_=topnav_storetab_kstore_cmx&_encoding=UTF8&slot=desktop-atf-10'
 response = requests.get(URL)
 html = response.content
@@ -17,24 +15,33 @@ KEY_ID = '' # Your AWS access key ID
 SECRET_KEY = '' # Your AWS secret access key
 
 
-# Transform
-new_releases = []
 
+"""Transforming: Cleaning the data"""
+# Function finds the data in the specified 'tag' and 'class'
 def find_data(tag, attr):
     return soup.find_all(tag, class_=attr)
 
+# Function that converts the data into strings
 def find_text(text):
     return str(text.get_text()).strip()
 
+# Function is for removing the unnecessary spaces and newlines from 'div' tags for the author data
 def find_author(name):
     return name.replace('by', '').replace(',', ' ,').split(',')[0].replace('\n', '').lstrip().rstrip()
 
+# Holds all the 'div' tags of the comics that will be counted
+number_of_comics = [find_text(w) for w in find_data('div', 'csw-grid-list-item-wrapper csw-inline-buy-trigger')]
+num = len(number_of_comics)
+
+# Holds the titles for each comic
 titles = [find_text(title) for title in find_data('span', 'csw-title-text')]
 
+# Holds the cover arts' image source for each comic
 cover_arts = [cover_art['src'] for cover_art in find_data('img', 'csw-item-art')]
-
+# Holds the author for each comic
 authors = [find_author(find_text(author)) for author in find_data('div', 'a-row csw-sub-title-row')]
 
+# Holds the required data (currency, dollar, and cent) to develop a price for each comic
 prices = []
 currency = [find_text(cu) for cu in find_data('span', 'csw-item-price-currency')]
 dollars = [find_text(d) for d in find_data('span', 'csw-item-price-dollars')]
@@ -42,48 +49,46 @@ cents = [find_text(ct) for ct in find_data('span', 'csw-item-price-cents')]
 
 # Function is for formating the price of a comic correctly with right the currency, dollar, and cent amount
 def get_prices(cur, dol, cnt):
-    for i in range(len(cur)):
+    for i in range(num):
         prices.append(f'{cur[i]}{dol[i]}.{cnt[i]}')
 
 get_prices(currency, dollars, cents)
 
+# Holds all the data (Title, Author, Cover Art, and Price) for each comic
+new_releases = []
 
 # Function is used to add the data into the 'new_releases' list with the comic's title, author, cover art, and its price
-def get_new_releases(titl, aut, img, pri):
-    for i in range(len(titl)):
-        new_releases.append([titl[i], aut[i], img[i], pri[i]])
+def get_new_releases(t, a, ca, p):
+    for i in range(num):
+        new_releases.append([t[i], a[i], ca[i], p[i]])
 
 get_new_releases(titles, authors, cover_arts, prices)
 
-# print(new_releases[0])
 
-# Loading the data into a predefined bucket called 'comixology-new-releases' in AWS S3
+
+"""Loading the data into a predefined bucket called 'comixology-new-releases' in AWS S3"""
 header = ['Title', 'Author', 'Cover Art', 'Price']
 today = datetime.date.today()
-filename = f'{today}.csv'
+filename = f'/tmp/{today}.csv' # 
 bucket = 'comixology-new-releases'
 
-
-
+# Retrieving the service source 
 s3 = boto3.client('s3',
             region_name='us-east-1',
             aws_access_key_id=KEY_ID,
             aws_secret_access_key=SECRET_KEY)
 
 
+# Creating a CSV file
 with open(f'{filename}', 'w') as file:
     writer = csv.writer(file)
     writer.writerow(header)
+    # Loading the data into the CSV file that was created
     for i in new_releases:
         writer.writerow(i)
 
+# Closing the CSV file
 file.close()
 
-
-
-
-s3.upload_file(filename, bucket, filename)
-    # r = s3.list_objects_v2(Bucket=bucket)
-    # print(r['Contents'][0]['Key'])
-
-
+# # Uploading the CSV file to S3
+# s3.upload_file(filename, bucket, filename)
